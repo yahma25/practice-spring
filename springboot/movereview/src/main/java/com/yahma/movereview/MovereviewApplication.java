78eaaa686a780e6d10426657f14b6dcc926c997b
mhkim <yahma25@gmail.com>
2021-06-14 21:13:04 +0900

    Spring Initializr 플러그인을 사용하여 초기 개발환경을 설정함.
    * Gradle Project.
    * 스프링부트 버전 2.5.1
    * Java 11
    * 패키지 종류 - Jar
    * 라이브러리 3개(Spring Boot DevTools, Lombok, Spring Web)

---
+++ springboot/movereview/src/main/java/com/yahma/movereview/MovereviewApplication.java
@@ -0,0 +1,13 @@
+package com.yahma.movereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovereviewApplication.class, args);
+	}
+
+}
---
+++ springboot/movereview/src/test/java/com/yahma/movereview/MovereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahma.movereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
