e298a20940a005cf37176f963bdfb30940e8c057
mhkim <yahma25@gmail.com>
2021-06-17 21:57:09 +0900

    JPA 연결을 위한 Memo 엔티티 클래스 추가

---
+++ src/main/java/com/yahma/entity/Memo.java
@@ -0,0 +1,30 @@
+package com.yahma.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "tbl_memo")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+}
