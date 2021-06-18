f47c5026857f56f07ec2591acf0225a6c2d7364e
mhkim <yahma25@gmail.com>
2021-06-18 22:29:17 +0900

    #3 프로젝트 실행 시, Entity로 등록한 클래스의 형태로 SQL을 동작시켜 테이블을 생성시키게 함

--- src/main/java/com/yahma/movie_review/MovieReviewApplication.java
+++ src/main/java/com/yahma/movie_review/MovieReviewApplication.java
@@ -2,8 +2,10 @@ package com.yahma.movie_review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.yahma.entity"})
 public class MovieReviewApplication {
 
     public static void main(String[] args) {
