1d43ff4cd88bd33cb39bec33f685d55fef91a1e8
mhkim <yahma25@gmail.com>
2021-06-18 23:55:09 +0900

    #3 MariaDB에서 MySQL DB로 변경

--- src/main/java/com/yahma/movie_review/MovieReviewApplication.java
+++ src/main/java/com/yahma/movie_review/MovieReviewApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.yahma.entity"})
+@EntityScan(basePackages = {"com.yahma.movie_review.entity"})
 public class MovieReviewApplication {
 
     public static void main(String[] args) {
