0d3da4e67c58cf2f8786fa4cd068bb5f44fd9904
mhkim <yahma25@gmail.com>
2021-06-19 09:42:26 +0900

    #3 삽입/조회/수정/삭제 테스트코드 추가

--- src/test/java/com/yahma/movie_review/repository/MemoRepositoryTests.java
+++ src/test/java/com/yahma/movie_review/repository/MemoRepositoryTests.java
@@ -1,8 +1,14 @@
 package com.yahma.movie_review.repository;
 
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import com.yahma.movie_review.entity.Memo;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class MemoRepositoryTests {
@@ -13,5 +19,50 @@ public class MemoRepositoryTests {
     @Test
     public void testClass() {
         System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect() {
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+        System.out.println("====================");
+
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testSelect2() {
+        Long mno = 100L;
+
+        Memo memo = memoRepository.getOne(mno);
+        System.out.println("====================");
+
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testUpdate() {
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+        System.out.println(memoRepository.save(memo));
+    }
+
+    @Test
+    public void testDelete() {
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
     }
 }
