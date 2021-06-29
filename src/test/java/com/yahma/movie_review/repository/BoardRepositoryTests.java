4989fc5e195b3580756098c8fc110c8f2cd3c6b8
mhkim <yahma25@gmail.com>
2021-06-29 22:07:24 +0900

    #22 @Query 사용하여 조인 조회 테스트코드 추가

--- src/test/java/com/yahma/movie_review/repository/BoardRepositoryTests.java
+++ src/test/java/com/yahma/movie_review/repository/BoardRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.yahma.movie_review.repository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -11,6 +13,10 @@ import com.yahma.movie_review.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 public class BoardRepositoryTests {
@@ -40,5 +46,44 @@ public class BoardRepositoryTests {
 
         System.out.println(board);
         System.out.println(board.getWriter());
+    }
+
+    @Test
+    public void testReadWithWriter() {
+        Object result = boardRepository.getBoardWithWriter(100L);
+
+        Object[] arr = (Object[])result;
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testGetBoardWithReply() {
+        List<Object[]> result = boardRepository.getBoardWithReply(76L);
+
+        for (Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    @Test
+    public void testWithReplyCount() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+            Object[] arr = (Object[])row;
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    public void testRead3() {
+        Object result = boardRepository.getBoardByBno(76L);
+
+        Object[] arr = (Object[])result;
+
+        System.out.println(Arrays.toString(arr));
     }
 }
