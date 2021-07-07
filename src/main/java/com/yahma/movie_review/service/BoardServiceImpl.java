66ecb739b1f3e779aa44aece8f81295438bad5a9
mhkim <yahma25@gmail.com>
2021-07-07 21:56:05 +0900

    #22 게시물 삭제 추가 (댓글 포함)

--- src/main/java/com/yahma/movie_review/service/BoardServiceImpl.java
+++ src/main/java/com/yahma/movie_review/service/BoardServiceImpl.java
@@ -2,12 +2,15 @@ package com.yahma.movie_review.service;
 
 import java.util.function.Function;
 
+import javax.transaction.Transactional;
+
 import com.yahma.movie_review.dto.BoardDTO;
 import com.yahma.movie_review.dto.PageRequestDTO;
 import com.yahma.movie_review.dto.PageResultDTO;
 import com.yahma.movie_review.entity.Board;
 import com.yahma.movie_review.entity.Member;
 import com.yahma.movie_review.repository.BoardRepository;
+import com.yahma.movie_review.repository.ReplyRepository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
@@ -21,6 +24,8 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class BoardServiceImpl implements BoardService {
     private final BoardRepository repository;
+
+    private final ReplyRepository replyRepository;
 
     @Override
     public Long register(BoardDTO dto) {
@@ -53,5 +58,13 @@ public class BoardServiceImpl implements BoardService {
         Object[] arr = (Object[])result;
 
         return entityToDTO((Board)arr[0], (Member)arr[1], (Long)arr[2]);
+    }
+
+    @Transactional
+    @Override
+    public void removeWithReplies(Long bno) {
+        replyRepository.deleteByBno(bno);
+
+        repository.deleteById(bno);
     }
 }
