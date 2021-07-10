b0afb4bfdfceaaed721b902b5a2c1d1a11a5c287
mhkim <yahma25@gmail.com>
2021-07-10 21:59:30 +0900

    #22 게시물 조회 추가

--- src/main/java/com/yahma/movie_review/controller/BoardController.java
+++ src/main/java/com/yahma/movie_review/controller/BoardController.java
@@ -7,6 +7,7 @@ import com.yahma.movie_review.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -44,5 +45,16 @@ public class BoardController {
         redirectAttributes.addFlashAttribute("msg", bno);
 
         return "redirect:/board/list";
+    }
+
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model) {
+        log.info("bno: " + bno);
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info("boardDTO: " + boardDTO);
+
+        model.addAttribute("dto", boardDTO);
     }
 }
