9c33d2aa811fbfd649a37f3c20be7c265cc94b19
mhkim <yahma25@gmail.com>
2021-07-18 22:51:00 +0900

    #35 댓글 수정 기능 추가

--- src/main/java/com/yahma/movie_review/controller/ReplyController.java
+++ src/main/java/com/yahma/movie_review/controller/ReplyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,13 @@ public class ReplyController {
 		Long rno = replyService.register(replyDTO);
 
 		return new ResponseEntity<>(rno, HttpStatus.OK);
+	}
+
+	@PutMapping("/{rno}")
+	public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO) {
+		replyService.modify(replyDTO);
+
+		return new ResponseEntity<>("success", HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{rno}")
