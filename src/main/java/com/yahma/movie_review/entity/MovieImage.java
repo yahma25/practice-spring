47cfb63e0701ae7898a4e6eb5d766a48f5a87376
mhkim <yahma25@gmail.com>
2021-07-20 22:52:46 +0900

    #38 영화 썸네일 이미지 정보를 저장할 클래스 추가

---
+++ src/main/java/com/yahma/movie_review/entity/MovieImage.java
@@ -0,0 +1,35 @@
+package com.yahma.movie_review.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString(exclude = "movie")
+public class MovieImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+}
