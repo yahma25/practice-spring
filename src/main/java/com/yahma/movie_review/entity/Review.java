6c01f51af3b7bf4a963ee73fb6a6c48febf2f9ad
mhkim <yahma25@gmail.com>
2021-07-20 23:38:36 +0900

    #38 리뷰 정보를 저장하는 클래스 추가

---
+++ src/main/java/com/yahma/movie_review/entity/Review.java
@@ -0,0 +1,36 @@
+package com.yahma.movie_review.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"movie", "memberForReview"})
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewnum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MemberForReview memberForReview;
+
+    private int grade;
+
+    private String text;
+}
