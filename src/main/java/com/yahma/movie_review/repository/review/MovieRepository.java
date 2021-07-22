afad282e7a96401a0f3501032a177be8c6202a04
mhkim <yahma25@gmail.com>
2021-07-22 22:29:45 +0900

    #38 영화화 연결된 첫 이미지만 포함한 쿼리로 변경

--- src/main/java/com/yahma/movie_review/repository/review/MovieRepository.java
+++ src/main/java/com/yahma/movie_review/repository/review/MovieRepository.java
@@ -8,7 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    @Query("select m, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
+         + "left outer join MovieImage mi on mi.movie = m "
          + "left outer join Review r on r.movie = m group by m")
+    // max(mi) 때문에 한 영화에 대한 지정된 개수만큼 이미지를 가져오느라 성능 저하 발생
+    // @Query("select m, max(mi), avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
+    //      + "left outer join MovieImage mi on mi.movie = m "
+    //      + "left outer join Review r on r.movie = m group by m")
     Page<Object[]> getListPage(Pageable pageable);
 }
