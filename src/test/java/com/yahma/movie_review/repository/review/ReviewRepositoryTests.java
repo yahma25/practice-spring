68cebdfdfc684e4f906e1447ae89b51db1bc6c18
mhkim <yahma25@gmail.com>
2021-07-22 23:23:34 +0900

    #38 지정한 영화에 대한 리뷰자 리스트 조회

--- src/test/java/com/yahma/movie_review/repository/review/ReviewRepositoryTests.java
+++ src/test/java/com/yahma/movie_review/repository/review/ReviewRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.yahma.movie_review.repository.review;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import com.yahma.movie_review.entity.review.MemberForReview;
@@ -31,6 +32,29 @@ public class ReviewRepositoryTests {
                     .build();
 
             reviewRepository.save(movieReview);
+        });
+    }
+
+    /**
+     * 세션 오류 발생
+     * - org.hibernate.LazyInitializationException: could not initialize proxy - no Session
+     * Review의 Member가 FetchType.LAZY 방식으로 되어 있음
+     * '@Transactional' 적용해도 .getMemberForReview().getEmail() 처리할 때마다 객체 로딩 발생.
+     * ReviewRepository.findByMovie()에서  @EntityGraph 방법으로 해결.
+     */
+    @Test
+    public void testGetMovieReviews() {
+
+        Movie movie = Movie.builder().mno(100L).build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+            System.out.println(movieReview.getReviewnum());
+            System.out.println("\t" + movieReview.getGrade());
+            System.out.println("\t" + movieReview.getText());
+            System.out.println("\t" + movieReview.getMemberForReview().getEmail());
+            System.out.println("---------------------------");
         });
     }
 }
