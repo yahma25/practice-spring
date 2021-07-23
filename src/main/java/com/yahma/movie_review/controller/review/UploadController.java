7d0c4c9c8e0a12c9e455b17727a47dd53cf0094c
mhkim <yahma25@gmail.com>
2021-07-23 17:43:54 +0900

    #38 파일 업로드 기능 적용

---
+++ src/main/java/com/yahma/movie_review/controller/review/UploadController.java
@@ -0,0 +1,74 @@
+package com.yahma.movie_review.controller.review;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+public class UploadController {
+
+    @Value("${com.yahma.movie_review.upload.path}") // application.properties 업로드 경로 변수
+    private String uploadPath;
+    
+    @PostMapping("/uploadAjax")
+    public void uploadFile(MultipartFile[] uploadFiles) {
+        for (MultipartFile uploadFile : uploadFiles) {
+            
+            // 이미지 파일만 업로드 가능
+            if (!uploadFile.getContentType().startsWith("image")) {
+                log.info("This file is not image type");
+                return;
+            }
+            
+            // 파일 원본 이름. IE/Edge는 전체 경로가 들어옴
+            String originalName = uploadFile.getOriginalFilename();
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            log.info("fileName: " + fileName);
+
+            String folderPath = makeFolder();
+            log.info("folderPath: " + folderPath);
+
+            String uuid = UUID.randomUUID().toString();
+
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+            log.info("saveName: " + saveName);
+
+            Path savePath = Paths.get(saveName);
+            log.info("savePath: " + savePath);
+
+            try {
+                uploadFile.transferTo(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if (!uploadPathFolder.exists()) {
+            uploadPathFolder.mkdirs();
+        }
+
+        return folderPath;
+    }
+}
