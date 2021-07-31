5d03788be9811ffbcc16cc7557131ecf421e9811
mhkim <yahma25@gmail.com>
2021-07-31 20:51:02 +0900

    #50 영화 조회 페이지 추가

--- src/main/java/com/yahma/movie_review/service/Movie/MovieServiceImpl.java
+++ src/main/java/com/yahma/movie_review/service/Movie/MovieServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yahma.movie_review.service.Movie;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -62,5 +63,31 @@ public class MovieServiceImpl implements MovieService {
         ));
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public MovieDTO getMovie(Long mno) {
+        
+        List<Object[]> result = movieRepository.getMovieWithAll(mno);
+
+        log.info("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ result ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        log.info("result 0번째: ", result.get(0));
+
+        // Movie Entity는 가장 앞에 존재 - 모든 Row가 같은 값
+        Movie movie = (Movie) result.get(0)[0];
+        log.info("movie", movie);
+
+        // 영화의 이미지 개수만큼 객체 필요
+        List<MovieImage> movieImages = new ArrayList<>();
+
+        result.forEach(arr -> {
+            MovieImage movieImage = (MovieImage) arr[1];
+            movieImages.add(movieImage);
+        });
+
+        Double avg = (Double) result.get(0)[2]; // 평균 평점
+        Long reviewCount = (Long) result.get(0)[3]; // 리뷰 개수
+
+        return entitiesToDTO(movie, movieImages, avg, reviewCount);
     }
 }
